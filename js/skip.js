2df723eaf0473b81516913de1359dd932fedf534
Joe Time <joetime@gmail.com>
2015-03-05 17:01:40 -0500

    avoid angular errors when input is undefine

--- js/skip.js
+++ js/skip.js
@@ -3,6 +3,7 @@
 angular.module('platanus.skip-filter', [])
   .filter('skip', function () {
     return function (input, skipCount) {
+      if (!input) return input;
       return input.slice(skipCount);
     };
   });
